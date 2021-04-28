eb1d672af0cbce8eb2ef42d3b5e00b08b47bca2c
Freditansari <Freditansari@users.noreply.github.com>
2021-04-28 02:13:41 -0400

    changed example name

--- graph-callbacks-dash-example.py
+++ graph-callbacks-dash-example.py
@@ -15,6 +15,8 @@ year_options =[]
 for year in df['year'].unique():
     year_options.append({'label': str(year), 'value': year})
 
+# print(year_options)
+
 app.layout = html.Div([
     dcc.Graph(id='graph'),
     dcc.Dropdown(id='year-picker', options =year_options, value=df['year'].min())
@@ -29,17 +31,24 @@ def update_figure(selected_year):
 
     traces = []
 
-    # for continent_name in filtered_df['continent'].unique():
-    #     df_by_continent = filtered_df[filtered_df['continent']==continent_name]
-    #     traces.append(go.Scatter(
-    #         x= df_by_continent['gdpPercap'],
-    #         y= df_by_continent['lifeExp'],
-    #         mode = 'markers', 
-    #         # opacity=0.7,
-    #         # marker = {'size': 15},
-    #         # name=continent_name
-    #     ))
-    return
+    for continent_name in filtered_df['continent'].unique():
+        df_by_continent = filtered_df[filtered_df['continent']==continent_name]
+        traces.append(go.Scatter(
+            x= df_by_continent['gdpPercap'],
+            y= df_by_continent['lifeExp'],
+            mode = 'markers', 
+            opacity=0.7,
+            marker = {'size': 15},
+            name=continent_name
+        ))
+    return {'data': traces, 
+    'layout': go.Layout(
+        title='My Plot', 
+        xaxis ={'title': 'GDP per Capita', 
+        'type': 'log'},
+        yaxis = {'title': 'Life Expectancy'}
+        
+        )}
 
 
 if __name__ == '__main__':
